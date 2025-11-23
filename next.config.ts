42ce88d66eaf0e2e3c87f95c3814e2edfef59c07
siddharthnag-source <siddharth.nag@himeera.com>
2025-11-23 20:20:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,33 +10,14 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/sign-in',
-        permanent: false,
-      },
-    ];
-  },
-
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
         ],
       },
       {
